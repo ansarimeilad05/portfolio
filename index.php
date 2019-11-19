21e04ba0176f1a11679c0c3e6a41fd75f5cfe404
”ansarimeilad05” <ansarimeilad@gmail.com>
2019-11-19 17:41:21 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+  <html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Meilad Ansari Per.2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Ansari_AboutMe.html"> Ansari_AboutMe.html</a></li></br>
+      <li><a href="Ansari_Projects.html">Ansari_ Projects.html</a></li></br>
+      <li><a href="Ansari_Resume.html"> Ansari_Resume.html</a></li></br>
+      <li><a href="Ansari_ContactMe.html"> Ansari_ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
